0620bd298a6353f317ee107cfcffd3a1d74f3cec
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-10-21 15:18:36 -0600

    newuser route works

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,7 +6,7 @@ const {
 } = require('../../controllers/userController');
 
 //     /api/users
-router.route('/').get(getUsers);
+router.route('/').get(getUsers).post(createUser);
 
 //    /api/users/:userId
 router.route('/:userId').get(getSingleUser);

a09445e95f9ce7dc58a609c03a1e366b56c17352
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-10-25 12:50:23 -0600

    get thought by id works

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -24,7 +24,23 @@ async function createThought(req, res) {
   }
 }
 
+// Get thought by ID
+async function getSingleThought(req, res) {
+  try {
+    const thought = await Thought.findOne({ _id: req.params.thoughtId });
+    if (!thought) {
+      return res.status(404).json({ message: 'No thought with that ID' });
+    }
+    return res.status(200).json(thought);
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
 module.exports = {
   getThoughts,
   createThought,
+  getSingleThought,
+  //   updateThought,
+  //   deleteThought,
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,10 +2,18 @@ const router = require('express').Router();
 const {
   getThoughts,
   createThought,
+  getSingleThought,
+  //   updateThought,
+  //   deleteThought,
 } = require('../../controllers/thoughtController');
 
 //     /api/thoughts
 // Get all thoughts and create new thought
 router.route('/').get(getThoughts).post(createThought);
+
+// Get a single thought by ID
+router.route('/:thoughtId').get(getSingleThought);
+//   .put(updateThought)
+//   .delete(deleteThought);
 
 module.exports = router;
